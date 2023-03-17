2bb52cbec6353521dc0d0bda4d88e772d1c32702
Andrew Dubov <40332480+Magikman008@users.noreply.github.com>
2023-03-17 22:22:10 +0300

    Create Counter Remover.user.js

---
+++ Counter Remover.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Counter Remover
+// @namespace    http://vk.com/just_sh1ne
+// @version      1
+// @description  The script removes the number of notifications from the title
+// @author       Magikman008
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @downloadURL  https://github.com/Magikman008/Counter-Remover/raw/main/Counter%20Remover.user.js
+// @updateURL    https://github.com/Magikman008/Counter-Remover/raw/main/Counter%20Remover.user.js
+// @grant        none
+// ==/UserScript==
+
+window.onload = function() {
+    var titleEl = document.getElementsByTagName("title")[0];
+    var docEl = document.documentElement;
+
+    if (docEl && docEl.addEventListener) {
+        docEl.addEventListener("DOMSubtreeModified", function(evt) {
+            var t = evt.target;
+            if (t === titleEl || (t.parentNode && t.parentNode === titleEl)) {
+                document.title = document.title.replace(/^\(\d.*\)/g, "");
+            }
+        }, false);
+    } else {
+        document.onpropertychange = function() {
+            if (window.event.propertyName == "title") {
+                document.title = document.title.replace(/\(\d.*\)/g, "");
+            }
+        };
+    }
+};
