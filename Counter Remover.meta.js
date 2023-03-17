307add071c5099111853a3583e674095011c9e76
Andrew Dubov <40332480+Magikman008@users.noreply.github.com>
2023-03-17 22:35:31 +0300

    Create Counter Remover.meta.js

---
+++ Counter Remover.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Counter Remover
+// @namespace    http://vk.com/just_sh1ne
+// @version      1.0.3
+// @description  The script removes the number of notifications from the title
+// @author       Magikman008
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @downloadURL  https://github.com/Magikman008/Counter-Remover/raw/main/Counter%20Remover.user.js
+// @updateURL    https://github.com/Magikman008/Counter-Remover/raw/main/Counter%20Remover.meta.js
+// @grant        none
+// ==/UserScript==
+
+{
+  "name": "Counter Remover",
+  "version": "1.0.3",
+  "description": "The script removes the number of notifications from the title",
+  "author": "Magikman008",
+  "update_url": "https://github.com/Magikman008/Counter-Remover/raw/main/Counter%20Remover.user.js"
+}
